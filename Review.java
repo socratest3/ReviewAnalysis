5bd594259296ed0116c9b1d0cdc86b85f9fe958e
socratest3 <socratest3@nycstudents.net>
2025-05-01 12:16:16 -0400

    Update Review.java

--- Review.java
+++ Review.java
@@ -1,1 +1,27 @@
+public class Review 
+{
+    private int rating;
+    private String comment;
+
+
+    /**
+     * Precondition: r >= 0
+     * c is not null.
+     */
+    public Review(int r, String c) 
+    {
+        rating = r;
+        comment = c;
+    }
+
+    public int getRating() 
+    {
+        return rating;
+    }
+
+    public String getComment() 
+    {
+        return comment;
+    }
+}
 
