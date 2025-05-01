fa3f958de0bacd20575d5a6af58299da9a4e5d14
socratest3 <socratest3@nycstudents.net>
2025-05-01 12:15:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,21 @@
+import java.util.ArrayList;
 
+public class Main
+{
+
+public static void main(String[] args)
+{
+    Review[]reviews = 
+    {
+        new Review(4,"Good! Thx"),
+        new Review(3, "Okay site"),
+        new Review(5, "Great!"),
+        new Review(2, "Poor! bad."),
+        new Review(3, "")
+    };
+    ReviewAnalysis r = new ReviewAnalysis(reviews);
+    System.out.println(r.getAverageRating());
+
+    System.out.println(r.collectComments());
+}
+}
